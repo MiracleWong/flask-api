81e64bc5ce5fc8bd90d20fdfa245b5526476bbd3
MiracleWong <cfwr1991@126.com>
2019-02-08 18:29:07 +0800

    验证Token

--- ginger/app/api/v1/user.py
+++ ginger/app/api/v1/user.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 from app.libs.redprint import Redprint
+from app.libs.token_auth import auth
 
 __author__ = 'MiracleWong'
 
@@ -7,7 +8,7 @@ api = Redprint('user')
 
 
 @api.route('', methods=["GET"])
-# @auth.login_required
+@auth.login_required
 def get_user():
     return 'get user'
 
--- ginger/app/libs/token_auth.py
+++ ginger/app/libs/token_auth.py
@@ -3,14 +3,43 @@
 created by MiracleWong on 2019/2/8
 """
 
+from collections import namedtuple
+from flask import current_app, g, request
 from flask_httpauth import HTTPBasicAuth
+from itsdangerous import BadSignature, SignatureExpired
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+
+from app.libs.error_code import AuthFailed
 
 __author__ = 'MiracleWong'
 
 
 auth = HTTPBasicAuth()
+User = namedtuple('User', ['uid', 'ac_type', 'scope'])
 
 
 @auth.verify_password
-def verify_password(account, password):
-    return "Hello World"
+def verify_password(token, password):
+    user_info = verify_auth_token(token)
+    if not user_info:
+        return False
+    else:
+        g.user = user_info
+        return True
+
+
+def verify_auth_token(token):
+    s = Serializer(current_app.config['SECRET_KEY'])
+    try:
+        data = s.loads(token)
+    except BadSignature:
+        # token无法解密的异常
+        raise AuthFailed(msg='token is invalid', error_code=1002)
+    except SignatureExpired:
+        # token时间过期的异常
+        raise AuthFailed(msg='token is expired', error_code=1003)
+
+    uid = data['uid']
+    ac_type = data['type']
+    return User(uid, ac_type, '')
+
