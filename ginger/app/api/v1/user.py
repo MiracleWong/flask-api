74788670ac679aaa789ba6064564198f9e35d8e2
MiracleWong <cfwr1991@126.com>
2019-02-08 18:35:53 +0800

    重写get_or_404 && first_or_404

--- ginger/app/api/v1/user.py
+++ ginger/app/api/v1/user.py
@@ -1,16 +1,24 @@
 # -*- coding:utf-8 -*-
+from flask import jsonify
+
 from app.libs.redprint import Redprint
 from app.libs.token_auth import auth
+from app.modules.user import User
 
 __author__ = 'MiracleWong'
 
 api = Redprint('user')
 
 
-@api.route('', methods=["GET"])
+@api.route('/<int:uid>', methods=["GET"])
 @auth.login_required
-def get_user():
-    return 'get user'
+def get_user(uid):
+    user = User.query.get_or_404(uid)
+    r = {
+        'nickname': user.nickname,
+        'email': user.email
+    }
+    return jsonify(r)
 
 
 @api.route('', methods=["POST"])
--- ginger/app/modules/user.py
+++ ginger/app/modules/user.py
@@ -38,9 +38,7 @@ class User(Base):
 
     @staticmethod
     def verify(email, password):
-        user = User.query.filter_by(email=email).first()
-        if not user:
-            raise NotFound(msg="user not found")
+        user = User.query.filter_by(email=email).first_or_404()
         if not user.check_password(password):
             print("password")
             raise AuthFailed()
