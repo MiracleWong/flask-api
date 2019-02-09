72f960a9340b8230746eb82ed508afd4e9bfb940
MiracleWong <cfwr1991@126.com>
2019-02-09 12:59:50 +0800

    支持权限相加，支持权限链式调用, 子类支持相加

--- ginger/app/libs/scope.py
+++ ginger/app/libs/scope.py
@@ -6,12 +6,35 @@ created by MiracleWong on 2019/2/9
 __author__ = 'MiracleWong'
 
 
-class AdminScope:
+class Scope:
+
+    def add(self, other):
+        self.allow_api = self.allow_api + other.allow_api
+        return self
+
+
+class AdminScope(Scope):
     allow_api = ['v1.super_get_user']
 
+    def __init__(self):
+        self.add(UserScope())
+        print(self.allow_api)
 
-class UserScope:
-    allow_api = ['v1.get_user']
+
+class UserScope(Scope):
+    allow_api = ['v1.A']
+
+
+class SuperScope(Scope):
+    allow_api = ['v1.C']
+
+    def __init__(self):
+        self.add(UserScope()).add(AdminScope())
+        print(self.allow_api)
+
+
+SuperScope()
+AdminScope()
 
 
 def is_in_scope(scope, endpoint):
