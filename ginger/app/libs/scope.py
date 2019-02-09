a3e14c666284a39b3a9992558c494b869f9d864f
MiracleWong <cfwr1991@126.com>
2019-02-09 14:17:00 +0800

    加入Forbidden形式

--- ginger/app/libs/scope.py
+++ ginger/app/libs/scope.py
@@ -9,6 +9,7 @@ __author__ = 'MiracleWong'
 class Scope:
     allow_api = []
     allow_module = []
+    forbidden = []
 
     def __add__(self, other):
         self.allow_api = self.allow_api + other.allow_api
@@ -16,6 +17,9 @@ class Scope:
 
         self.allow_module = self.allow_module + other.allow_module
         self.allow_module = list(set(self.allow_module))
+
+        self.forbidden = self.forbidden + other.forbidden
+        self.forbidden = list(set(self.forbidden))
         # 运算符重载
         return self
 
@@ -39,6 +43,8 @@ def is_in_scope(scope, endpoint):
     scope = globals()[scope]()
     splits = endpoint.split('+')
     red_name = splits[0]
+    if endpoint in scope.forbidden:
+        return False
     if endpoint in scope.allow_api:
         return True
     if red_name in scope.allow_module:
